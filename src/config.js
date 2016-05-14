38ca3cc6127c7e1bd9d35a93b6d406aeaa5b6105
Alexandru Rosianu <rosian_alex@yahoo.com>
2016-05-14 10:46:15 +0300

    Check in config.js

---
+++ src/config.js
@@ -0,0 +1,36 @@
+export default {
+  port: process.env.UPDATES_PORT || process.env.PORT || 3000,
+  host: process.env.UPDATES_HOST || '0.0.0.0',
+  github: {
+    token: process.env.GITHUB_TOKEN,
+    api: {
+      version: '3.0.0',
+      headers: {
+        'user-agent': 'Aluxian/squirrel-updates-server'
+      }
+    }
+  },
+  user: process.env.REPO_OWNER || 'Aluxian',
+  repo: process.env.REPO_NAME || 'Whatsie',
+  privateRepo: process.env.UPDATES_PRIVATE_REPO || false,
+  patterns: {
+    darwin: {
+      dmg: /-osx\.dmg/,
+      zip: /-osx\.zip/
+    },
+    win32: {
+      installer: /-win32-setup\.exe/,
+      zip: /-win32-portable\.zip/
+    },
+    linux: {
+      deb: {
+        i386: /-linux-i386\.deb/,
+        amd64: /-linux-amd64\.deb/
+      },
+      rpm: {
+        i386: /-linux-i386\.rpm/,
+        x86_64: /-linux-x86_64\.rpm/
+      }
+    }
+  }
+};
