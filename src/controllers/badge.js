3c87b185c33ceef4f7b0a52fcf5090dfd5b191ed
Alexandru Rosianu <rosian_alex@yahoo.com>
2016-05-15 15:31:53 +0300

    Add badge endpoint

---
+++ src/controllers/badge.js
@@ -0,0 +1,13 @@
+import stats from '../components/stats';
+import numeral from 'numeral';
+
+export async function main(req, res) {
+  const type = req.params.type;
+  if (!['downloads'].includes(type)) throw new Error(`400:Invalid type '${type}'.`);
+
+  const allStats = await stats.get();
+  const count = numeral(allStats.platforms_r.all).format('0.0a');
+  const link = 'https://img.shields.io/badge/downloads-' + count + '-brightgreen.svg';
+
+  res.redirect(302, link);
+}
--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ import * as homeCtrl from './controllers/home';
 import * as updateCtrl from './controllers/update';
 import * as downloadCtrl from './controllers/download';
 import * as statsCtrl from './controllers/stats';
+import * as badgeCtrl from './controllers/badge';
 
 const app = express();
 app.use(morgan('common'));
@@ -27,6 +28,7 @@ app.get('/update/:channel/win32/:file', asyncHandler(updateCtrl.win32_file));
 app.get('/update/:channel/linux', asyncHandler(updateCtrl.linux));
 app.get('/download/:platform/latest', asyncHandler(downloadCtrl.latest));
 app.get('/stats', asyncHandler(statsCtrl.main));
+app.get('/badge/:type.svg', asyncHandler(badgeCtrl.main));
 
 app.listen(config.port, config.host, () => {
   console.log('Server listening on port %s.', config.port);
